553293f9dffde0c1c4163e673181051b6f471831
Marcus Young <marcus@graspit.co.uk>
2024-05-24 10:57:44 +0100

    getting ready for hosted database

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,19 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
-const config = {};
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE not set or DATABASE_URL not set");
-}
+const config = {};
+
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+}
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE not set or DATABASE_URL not set");
 }
 
 module.exports = new Pool(config);
