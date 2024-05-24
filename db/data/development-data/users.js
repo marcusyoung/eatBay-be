bd8b9e47645b8bd0a92fbb3e96e8d8388a108a90
sofefaal@gmail.com <sofefaal@gmail.com>
2024-05-24 12:03:33 +0100

    added more users

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -4,7 +4,7 @@ module.exports = [
     password: "food123",
     name: "Keith",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: false,
   },
   {
@@ -12,7 +12,7 @@ module.exports = [
     password: "marcusisthebest",
     name: "Marcus",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -20,7 +20,7 @@ module.exports = [
     password: "lunar123",
     name: "Sofe",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -28,7 +28,7 @@ module.exports = [
     password: "purple123",
     name: "Jennifer",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -36,7 +36,7 @@ module.exports = [
     password: "purple123",
     name: "Verity",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -44,7 +44,7 @@ module.exports = [
     password: "mcdonalds",
     name: "Philip",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -52,7 +52,7 @@ module.exports = [
     password: "spicegirls",
     name: "Emily",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -60,7 +60,7 @@ module.exports = [
     password: "northcoders",
     name: "Taiwo",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -68,7 +68,7 @@ module.exports = [
     password: "lovegreggs",
     name: "Amanda",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -76,7 +76,7 @@ module.exports = [
     password: "benisthebest",
     name: "Ben",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -84,7 +84,7 @@ module.exports = [
     password: "alfarid",
     name: "Farid",
     avatar_url:
-         "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -92,7 +92,7 @@ module.exports = [
     password: "supermarket",
     name: "Aldi",
     avatar_url:
-        "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
   },
   {
@@ -147,6 +147,94 @@ module.exports = [
     email: "adam@northcoders.com",
     password: "adam1234",
     name: "Adam",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: true,
+  },
+  {
+    email: "hannah@northcoders.com",
+    password: "hannah1234",
+    name: "Hannah",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: false,
+  },
+  {
+    email: "sam@northcoders.com",
+    password: "sam1234",
+    name: "Sam",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: false,
+  },
+  {
+    email: "niko@northcoders.com",
+    password: "niko1234",
+    name: "Niko",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: true,
+  },
+  {
+    email: "josh@northcoders.com",
+    password: "josh1234",
+    name: "Josh",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: true,
+  },
+  {
+    email: "emil@northcoders.com",
+    password: "emil1234",
+    name: "Emil",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: false,
+  },
+  {
+    email: "michael@northcoders.com",
+    password: "michael1234",
+    name: "Michael",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: true,
+  },
+  {
+    email: "eva@northcoders.com",
+    password: "eva1234",
+    name: "Eva",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: false,
+  },
+  {
+    email: "eve@northcoders.com",
+    password: "eve1234",
+    name: "Eve",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: true,
+  },
+  {
+    email: "niamh@northcoders.com",
+    password: "niamh1234",
+    name: "Niamh",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: true,
+  },
+  {
+    email: "matt@northcoders.com",
+    password: "matt1234",
+    name: "Matt",
+    avatar_url:
+      "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
+    notifications: true,
+  },
+  {
+    email: "joyce@northcoders.com",
+    password: "joyce1234",
+    name: "Joyce",
     avatar_url:
       "https://secure.gravatar.com/avatar/210394d943f019b605e667c6538cddb0?s=512&d=mm&r=g",
     notifications: true,
